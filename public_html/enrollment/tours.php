ca69c7f3eac8d19b6bcfb6c48c4a7ad12c432098
Jason Thrasher <jasonthrasher@gmail.com>
2015-01-21 22:11:47 -0800

    Updated copy per Heather's request

--- public_html/enrollment/tours.php
+++ public_html/enrollment/tours.php
@@ -7,8 +7,17 @@
 
 <h2>Tours</h2>
 
-<p> You may set up an appointment for a tour or request additional information by emailing <a href="mailto:heather@geokids.org">heather@geokids.org</a> or contacting (650) 329-4236.  
+<!--
+<p> You may set up an appointment for a tour or request additional information by emailing <a href="mailto:heather@geokids.org">heather@geokids.org</a> or contacting (650) 329-4236.  
 
 <p>Because visits during the school week can be disruptive, we don’t offer individual tours unless there is an immediate opening available during the school year. </p>
+-->
+<p>
+Due to the severity of the flu season all tours have been cancelled until further notice. Tours will resume in the Fall of 2015. Request additional information by emailing tours@geokids.org or contacting (650) 329-4236.
+</p>
+
+<p>
+Because visits during the school week can be disruptive, we do not offer individual tours unless there is an immediate opening available during the school year.
+</p>
 
 <? include("../content/pagebottom.inc"); ?>
