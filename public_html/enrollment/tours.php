f71d37287bc25bcd82d8e4a46b9cab9e15927659
Jason Thrasher <jasonthrasher@gmail.com>
2015-11-06 10:07:03 -0800

    Added tour signup form and removed unused text

--- public_html/enrollment/tours.php
+++ public_html/enrollment/tours.php
@@ -7,25 +7,6 @@
 
 <h2>Tours</h2>
 
-<<<<<<< HEAD
-<p> You may set up an appointment for a tour or request additional information by emailing <a href="mailto:heather@geokids.org">heather@geokids.org</a> or contacting (650) 329-4236.  
-
-<p>Because visits during the school week can be disruptive, we don't offer individual tours unless there is an immediate opening available during the school year</p>
-
 <iframe src="https://docs.google.com/forms/d/1N1R52H8pydnM8PL06yScyl2K5H-BI0Zghy5KGhB7v60/viewform?embedded=true#start=embed" width="540" height="900" frameborder="0" marginheight="0" marginwidth="0">Signup Loading...</iframe>
-=======
-<!--
-<p> You may set up an appointment for a tour or request additional information by emailing <a href="mailto:heather@geokids.org">heather@geokids.org</a> or contacting (650) 329-4236.  
-
-<p>Because visits during the school week can be disruptive, we don’t offer individual tours unless there is an immediate opening available during the school year. </p>
--->
-<p>
-Due to the severity of the flu season all tours have been cancelled until further notice. Tours will resume in the Fall of 2015. Request additional information by emailing tours@geokids.org or contacting (650) 329-4236.
-</p>
-
-<p>
-Because visits during the school week can be disruptive, we do not offer individual tours unless there is an immediate opening available during the school year.
-</p>
->>>>>>> cad7f8c9588a0ea92c9a8039b328d6a6874370ae
 
 <? include("../content/pagebottom.inc"); ?>
